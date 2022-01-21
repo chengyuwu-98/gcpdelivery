26bcee7ef41ce55f6db546ec57472a0f3ca9fdbe
Chengyu Wu <chengyu-wu1998@outlook.com>
2022-01-21 17:04:08 +0000

    adding project

---
+++ Makefile
@@ -0,0 +1,10 @@
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+lint: 
+	pylint --disable=R,C main.py
+test:
+	#python -m pytest -vv main.py
+
+all:install lint test
---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(0, "/home/wcy505823098/.delivery/lib/python3.7/site-packages")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Welcome to the web!'
+
+@app.route('/name/<value>')
+def name(value):
+    """Return a friendly HTTP greeting."""
+    return 'Welcome to the web,%s!' % value
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
