138d92a3a407600045a30509212ccba78d4dfabb
Chengyu Wu <chengyu-wu1998@outlook.com>
2022-01-22 16:23:48 +0000

    add

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def hello():
 @app.route('/name/<value>')
 def name(value):
     """Return a friendly HTTP greeting."""
-    return 'Welcome to the web, %s!' % value
+    return 'Welcome to the webpage, %s!' % value
 
 @app.route('/gender/<value>')
 def gender(value):
     """Return a friendly HTTP greeting."""
-    return 'Welcome to the web, %s!' % value
+    return 'Welcome to the webpage, %s!' % value
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
